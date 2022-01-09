a8c53420af0bb0a667973a217293b4f2577fa23d
Manuel Spagnolo <spagnolo.manu@gmail.com>
2022-01-09 14:31:34 +0100

    test: setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,15 @@
 module Main (main) where
 
+import System.Exit (exitFailure, exitSuccess)
+import Test.HUnit (Counts (failures), Test (..), assertEqual, runTestTT)
+
+test1 :: Test
+test1 = TestCase (assertEqual "label" "1" "2")
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = do
+  results <- runTestTT tests
+  if failures results > 0 then exitFailure else exitSuccess
